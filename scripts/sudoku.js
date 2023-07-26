172be6c369e7047aa9a4c6b8d7593d4bf64b85cc
Curtis Kauer <kauercurtis@gmail.com>
2023-07-26 00:22:17 -0700

    eventListeners-submitButton

---
+++ scripts/sudoku.js
@@ -0,0 +1,8 @@
+let submit = document.getElementById("input");
+
+window.addEventListener("load", () => {
+    let submit = document.getElementById("input");
+    submit.addEventListener("click", () => {
+        console.log("Data being submitted");
+    });
+});
