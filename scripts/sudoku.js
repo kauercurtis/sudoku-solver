a4ed2d22143a39793ba058167f2d2939d2e17d3f
Curtis Kauer <kauercurtis@gmail.com>
2023-07-28 00:59:31 -0700

    square - inputTags

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -4,5 +4,10 @@ window.addEventListener("load", () => {
     let submit = document.getElementById("input");
     submit.addEventListener("click", () => {
         console.log("Data being submitted");
+        let test = document.getElementById("square1");
+        console.log(square1.value);
+        test = document.getElementById("square2");
+        console.log(square2.value);
+        console.log(square3.value);
     });
 });
